b31839c4db804d049726b5a38e5c8c2e2a578588
JONNAGADDALA RAVIKUMAR <ravikumarjit583@gmail.com>
2020-11-27 11:16:42 +0530

    UC1 -> TC1.2.0 No Such File Exception

--- __test__/StateCensusAnalyser.test.js
+++ __test__/StateCensusAnalyser.test.js
@@ -1,6 +1,7 @@
 const readCsvFile = require('../main/IndiaStateAnalyser');
 
 const FILE_PATH = 'C:\\Users\\Admin\\javaScript\\IndiaStateCensusProject\\resources\\IndiaStateCensusData.csv';
+const WRONG_FILEPATH = 'C:\\Users\\Admin\\javaScript\\StateCensusProject\\resources\\IndiaStateCensusData.csv';
 
 //Happy Test Case
 test('This TestCase Pases When Return Value Equals to number of States', () => {
@@ -8,4 +9,12 @@ test('This TestCase Pases When Return Value Equals to number of States', () => {
     .then(function(count) {
         expect(count).toBe(29);
     })    
+})
+
+//Sad TestCase
+test('This TestCase Pases when Returned Exception is No Such File', () => {
+    readCsvFile(WRONG_FILEPATH)
+    .catch(function(error) {
+        expect(error.message).toBe('No Such File');
+    })   
 })
--- main/IndiaStateAnalyser.js
+++ main/IndiaStateAnalyser.js
@@ -5,23 +5,26 @@ const path = require('path');
 //Reading CSV file and Storing in an Array
 const readCsvFile = (filePath) => {
     const states = [];
-    return new Promise(function(resolve) {
-              
-        fs.createReadStream(filePath)
-        .pipe(csv())
-        .on('data', (row) => {
-            const stateData = {
-                State: row.State,
-                Population: row.Population,
-                AreaInSqKm: row.AreaInSqKm,
-                DensityPerSqKm: row.DensityPerSqKm
-            }
-                states.push(stateData); 
-        })
+    return new Promise(function(resolve, reject) {
+        if(!fs.existsSync(filePath)) {
+            reject(new Error('No Such File'));
+        } else {     
+            fs.createReadStream(filePath)
+            .pipe(csv())
+            .on('data', (row) => {
+                const stateData = {
+                    State: row.State,
+                    Population: row.Population,
+                    AreaInSqKm: row.AreaInSqKm,
+                    DensityPerSqKm: row.DensityPerSqKm
+                }   
+                    states.push(stateData); 
+            })
         
-        .on('end', () => {
-            resolve(states.length)
-        })
+            .on('end', () => {
+                resolve(states.length)
+            })
+        }
     })
 }
 module.exports = readCsvFile;
